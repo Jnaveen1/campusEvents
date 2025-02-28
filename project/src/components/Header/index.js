0f0c35fb767692aa94b586c8f02cfae49223a5cd
naveenJV <naveenjanapati65@gmail.com>
2025-02-28 15:43:56 +0530

    changes in header style

--- project/src/components/Header/index.js
+++ project/src/components/Header/index.js
@@ -29,20 +29,20 @@ const Header = () =>{
 
     return(
         <div className="header-container">
-    <img 
-        src="https://res.cloudinary.com/dkhdhl0aw/image/upload/v1740389540/DALL_E_2025-02-24_15.00.16_-_A_sophisticated_and_creative_logo_where_an_intricate_design_is_the_main_focus_with_the_letters_JVN_subtly_embedded_in_the_background._The_design_sh_fzi2hh.webp" 
-        alt="JVN Logo" 
-        className="logo"
-    />
-    <div className="header-buttons">
-        <button onClick={onClickHome}>Home</button>
-        <button onClick={onClickRegisteredEvents}>Registered Events</button>
-        {(userRole === "admin" || userRole === "organizer") && (
-            <button type="button" onClick={onClickMyEvents}>My Events</button>
-        )}
-        <button type="button" onClick={onClickLogout}>Logout</button>
-    </div>
-</div>
+            <img 
+                src="https://res.cloudinary.com/dkhdhl0aw/image/upload/v1740389540/DALL_E_2025-02-24_15.00.16_-_A_sophisticated_and_creative_logo_where_an_intricate_design_is_the_main_focus_with_the_letters_JVN_subtly_embedded_in_the_background._The_design_sh_fzi2hh.webp" 
+                alt="JVN Logo" 
+                className="logo"
+            />
+            <div className="header-buttons">
+                <button onClick={onClickHome}>Home</button>
+                <button onClick={onClickRegisteredEvents}>Registered Events</button>
+                {(userRole === "admin" || userRole === "organizer") && (
+                    <button type="button" onClick={onClickMyEvents}>My Events</button>
+                )}
+                <button type="button" onClick={onClickLogout}>Logout</button>
+            </div>
+        </div>
 
     )
 }
